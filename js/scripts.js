f715cdf40c72ef00076cb7e162a7252ba11c2d2a
Alex Siegberg and Garland Lai <alex@>
2019-06-05 16:08:11 -0700

    add form contact list

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 //this code will run after everything loads
 $(document).ready(function(){
+  $("form#survey").submit(function(event){
+    event.preventDefault();
+    var nameInput = $("#name").val();
+    var phoneInput = $("#phone").val();
+    var addressInput = $("#address").val();
+    var birthInput = $("#birth").val();
 
-
+    var contact = ("<h3>Name: " + nameInput +
+                   "</h3><ul><li>Phone: " + phoneInput +
+                   "</li><li>Address: " + addressInput +
+                   "</li><li>Birth: " + birthInput) +
+                   "</li></ul><hr>";
+    $(".output").append(contact);
+  });
 });
